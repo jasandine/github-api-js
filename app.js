3ed9a0b003dac7e4e18f02cf57630347dbd91abe
jasandine <jasandine@gmail.com>
2015-05-23 15:05:18 -0700

    Add functionality to poll for updates

--- app.js
+++ app.js
@@ -8,3 +8,16 @@
   $.getJSON("https://api.github.com/orgs/factor-io/issues?state=all&access_token=158e8f36a5f996eb4af68e713c6c28f6533f1814&filter=all", function (data) {
     viewModel.issues(data);
   });
+
+  (function poll() {
+    $.ajax({
+        url: "https://api.github.com/orgs/factor-io/issues?state=all&access_token=158e8f36a5f996eb4af68e713c6c28f6533f1814&filter=all",
+        type: "GET",
+        success: function(data) {
+            console.log("polling");
+        },
+        dataType: "json",
+        complete: setTimeout(function() {poll()}, 5000),
+        timeout: 2000
+    })
+  })();
